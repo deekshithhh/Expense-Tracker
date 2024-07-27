2276c72c6809c490a93484efcbcd1019d9f7930c
Deekshith <deekshith3538@gmail.com>
2024-07-28 01:54:35 +0530

    Top Section Logic Made

--- expensetracker/src/components/PieRepresentation.jsx
+++ expensetracker/src/components/PieRepresentation.jsx
@@ -32,21 +32,7 @@ const CustomExpenseLegend = ({ payload }) => {
 };
 
 const PieRepresentation = ({ data }) => {
-  const COLORS = [
-    "#A000FF",
-    "#FF9304",
-    "#FDE006",
-    "#FF5733",
-    "#33FF57",
-    "#3357FF",
-    "#FF33A1",
-    "#FF8C33",
-    "#33FFF7",
-    "#8C33FF",
-    "#FFD433",
-    "#33FF8C",
-    "#FF3333",
-  ];
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
   return (
     <>
       <ResponsiveContainer>
@@ -67,12 +53,35 @@ const PieRepresentation = ({ data }) => {
               />
             ))}
           </Pie>
-          <Legend content={CustomExpenseLegend} />
+         <Legend iconType="rect" verticalAlign="bottom" />
           <Tooltip />
         </PieChart>
       </ResponsiveContainer>
     </>
   );
+
+
+  //   return (
+  //   <ResponsiveContainer width="100%" height={250}>
+  //     <PieChart width={400} height={400}>
+  //       <Pie
+  //         data={data}
+  //         cx="50%"
+  //         cy="50%"
+  //         labelLine={false}
+          
+  //         outerRadius={80}
+  //         fill="#8884d8"
+  //         dataKey="value"
+  //       >
+  //         {data.map((entry, index) => (
+  //           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+  //         ))}
+  //       </Pie>
+  //       <Legend iconType="rect" verticalAlign="bottom" />
+  //     </PieChart>
+  //   </ResponsiveContainer>
+  // );
 };
 
 export default PieRepresentation;
